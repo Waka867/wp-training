01edb7162adf5c4c8b9824e3e65d437b9f60f40d
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2019-06-25 10:07:24 -0400

    WP Training Udemy course initial commit

---
+++ wp-content/themes/2.07-jointswp-starter/functions.php
@@ -0,0 +1,64 @@
+<?php
+// Theme support options
+require_once(get_template_directory().'/assets/functions/theme-support.php'); 
+
+// WP Head and other cleanup functions
+require_once(get_template_directory().'/assets/functions/cleanup.php'); 
+
+// Register scripts and stylesheets
+require_once(get_template_directory().'/assets/functions/enqueue-scripts.php'); 
+
+// Register custom menus and menu walkers
+require_once(get_template_directory().'/assets/functions/menu.php'); 
+
+// Register sidebars/widget areas
+require_once(get_template_directory().'/assets/functions/sidebar.php'); 
+
+// Makes WordPress comments suck less
+require_once(get_template_directory().'/assets/functions/comments.php'); 
+
+// Replace 'older/newer' post links with numbered navigation
+require_once(get_template_directory().'/assets/functions/page-navi.php'); 
+
+// Adds support for multiple languages
+require_once(get_template_directory().'/assets/translation/translation.php'); 
+
+
+// Remove 4.2 Emoji Support
+// require_once(get_template_directory().'/assets/functions/disable-emoji.php'); 
+
+// Adds site styles to the WordPress editor
+//require_once(get_template_directory().'/assets/functions/editor-styles.php'); 
+
+// Related post function - no need to rely on plugins
+// require_once(get_template_directory().'/assets/functions/related-posts.php'); 
+
+// Use this as a template for custom post types
+// require_once(get_template_directory().'/assets/functions/custom-post-type.php');
+
+// Customize the WordPress login menu
+// require_once(get_template_directory().'/assets/functions/login.php'); 
+
+// Customize the WordPress admin
+// require_once(get_template_directory().'/assets/functions/admin.php'); 
+
+/*
+// Create and load custom post type
+function portfolio_post_type_init() {
+ 
+	register_post_type('portfolio', array(
+	       	'labels' 	=> array(
+                	'name' 		=> __( 'Portfolio' ),
+                	'singular_name' => __( 'Portfolio' )
+            	),
+		'description' 	=> 'This is a test.',
+		'show_ui'	=> true,
+		'show_in_menu'	=> true,
+		'public' => true
+	));
+
+}
+
+// Connect action to the theme set-up hook
+add_action('init', 'portfolio_post_type_init');
+*/
---
+++ wp-content/themes/wphierarchy/functions.php
@@ -0,0 +1,47 @@
+
+<?php
+
+
+// Add theme support
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+add_theme_support( 'html5' );
+add_theme_support( 'automatic-feed-links' );
+add_theme_support( 'custom-background' );
+add_theme_support( 'custom-header' );
+add_theme_support( 'custom-logo' );
+add_theme_support( 'customize-selective-refresh-widgets' );
+add_theme_support( 'starter-content' );
+
+// Load CSS
+function wphierarchy_enqueue_styles() {
+	wp_enqueue_style( 'main-css', get_stylesheet_directory_uri . '/style.css', [], array(), 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_styles' );
+
+
+// Create and load custom post type
+function portfolio_post_type_init() {
+
+	register_post_type('portfolio', array(
+	       	'labels' 	=> array(
+                	'name' 		=> __( 'Portfolio' ),
+                	'singular_name' => __( 'Portfolio' )
+            	),
+		'description' 	=> 'This is a test.',
+		'show_ui'	=> true,
+		'show_in_menu'	=> true,
+		'public' => true
+	));
+
+}
+
+// Connect action to the theme set-up hook
+add_action('init', 'portfolio_post_type_init');
+
+
+
+
+
+?>
