8241f5ca393e69a452dc2bbfa16ccf4d42247460
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2019-12-16 16:11:48 -0500

    Styling and splitting off Castle Wackula theme to new repo comments styling changes banner styling tweaks moved custom theme to folder for upload to Castle Wackula repository removed practice-theme5.84 files

--- wp-content/themes/practice-theme5.84/assets/functionality.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(function(){
-
-  //console.log('test');
-
-  /*****Add script for dropdown menu*******/
-  
-
-});
--- wp-content/themes/practice-theme5.84/template-parts/site-banner.php
+++
@@ -1,5 +0,0 @@
-<div class="" id="custom-site-banner">
-	<h1 id="custom-site-banner-title"><?php bloginfo('name'); ?></h1>
-</div>
-
-
