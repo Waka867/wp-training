690be03ca74317ed1293108030564f37c876944f
Ledwing Hernandez <ledwing.hernandez@gmail.com>
2019-12-17 10:59:03 -0500

    Cleared out unused themes Corrected package.json scss compile options due to removal of practice-theme5.84 directory

--- wp-content/themes/4.18-post-common-wptags/template-parts/byline.php
+++
@@ -1,7 +0,0 @@
-<p class="byline">
-  Date: <?php the_time( 'm-j-Y' ); ?>
-  <?php esc_html_e( 'Categories: ', 'wptags' ); ?>
-  <?php //the_category( ', ', 'single' ); ?>
-  <?php the_category( ' | ' ); ?>
-  <?php the_tags( 'Tags: ', ', ', ' |' ); ?>
-</p>
